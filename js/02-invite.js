b9fdb948e75fb28ede927b9bd362903a1cbc7b8e
JiangShiHanWedding <94063846+JiangShiHanWedding@users.noreply.github.com>
2021-11-11 18:00:15 +0800

    Update 02-invite.js

--- js/02-invite.js
+++ js/02-invite.js
@@ -19,7 +19,7 @@ $(document).ready(function(){
 
     var navele = $("#nav");
     
-    if($(window).height()<670&& $(window).width()>700){
+    if($(window).height()<670&& $(window).width()>600){
         female.css("font-size","2em");
         male.css("font-size","2em");
         pic1.css("margin-top","0%");
