6823c8d7e705f1da378f6f9f2b6828a22048948d
JiangShiHanWedding <94063846+JiangShiHanWedding@users.noreply.github.com>
2021-11-14 01:46:47 +0800

    Update 10-problem.js

--- js/10-problem.js
+++ js/10-problem.js
@@ -104,7 +104,7 @@ $(document).ready(function(){
             //后台请求
             $.ajax({
                 type:"post",
-                url:"http://localhost:9990/problem",
+                url:"https://slikeglue.com:9990/wedding/problem",
                 dateType:"json",
                 data: content,
                 //消息请求类型
